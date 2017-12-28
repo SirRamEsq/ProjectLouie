dc2cca7ee460d8e6c7c6cd0ccf1fc4c490b018f4
SirRamESQ <gmrl95@gmail.com>
2017-12-28 09:05:22 -0500

    Add unit test for lua require
    Added REQUIRE_NO_THROW and REQUIRE_THROW to GS_Test
        lua scripts pass a callback to these functions
    
    Wrote test script that verifies the behaviour of require

---
+++ Resources/Scripts/Testing/Integration/RequireTest.lua
@@ -0,0 +1,33 @@
+local con = {}
+function con.NewState(baseclass)
+	local state = baseclass or {}
+
+	function state.Initialize()
+		state.depth		= state.LEngineData.depth;
+		state.parent	= state.LEngineData.parent;
+		state.EID		= state.LEngineData.entityID;
+		state.name = "NAME"
+	end
+
+	function state.Setup(testing)
+	end
+
+	function state.TestRequire(testing)
+		local function callback1()
+			local shouldHaveValue = require("Testing/Integration/RequireTest.lua")
+			testing:REQUIRE_NOT_EQUAL(shouldHaveValue, nil)
+		end
+		testing:REQUIRE_NO_THROW(callback1)
+
+		local function callback2()
+			local shouldCrash = require ("_sdfewsa/_frg_nonsneas.notluaatall")
+		end
+		testing:REQUIRE_THROW(callback2)
+	end
+
+	state.TESTS = {state.TestRequire}
+
+	return state;
+end
+
+return con.NewState;
